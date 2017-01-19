84afd49bf7e2aed016adef8300c7a23790010f97
Andrew Long <andrew.aeontek@gmail.com>
2017-01-19 00:17:53 -0600

    Scaffolding & a couple of basic methods.

---
+++ PracticeCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PracticeCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index()
+        {
+            return "abacus";
+        }
+
+        public double Add(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+    }
+}
---
+++ PracticeCalculator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PracticeCalculator.MvcApplication" Language="C#" %>
